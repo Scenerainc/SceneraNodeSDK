bfc3534ea86ea99ada707108d955e6166cfe60eb
Dirk Meulenbelt <dirk.meulenbelt@scenera.net>
2023-04-04 12:39:38 +0200

    Classifier changed

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10.10",
-        "Programming Language :: Python :: 3.11.2"
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent"
     ],
     packages=["scenera.node"],
